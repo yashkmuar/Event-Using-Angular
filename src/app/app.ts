4e107858353e949262d9a1a6543f4ab9f40089cf
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-04 18:22:04 +0530

    focus,blur,input events

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,10 +10,11 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title:string = 'event-using-angular';
 
-  handleEvent(event:MouseEvent){
+  handleEvent(event:Event){
     console.log('Function called',event.type);
     console.log('Function called',event.target);
     console.log('Function called', event.target ? (event.target as HTMLInputElement).name : undefined);
     console.log('Function called',(event.target as Element).className);
+    console.log('Event Value', (event.target as HTMLInputElement)?.value);
   }
 }
