5f1a507b8e16483ec2341c10c59198bf66636d2c
Yash Kumar <jitendrakumar1988w@gmail.com>
2025-11-04 18:12:03 +0530

    mouse enter event with box type called

--- src/app/app.ts
+++ src/app/app.ts
@@ -10,10 +10,10 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title:string = 'event-using-angular';
 
-  handleEvent(event:any){
+  handleEvent(event:MouseEvent){
     console.log('Function called',event.type);
     console.log('Function called',event.target);
-    console.log('Function called',event.target.name);
+    console.log('Function called', event.target ? (event.target as HTMLInputElement).name : undefined);
     console.log('Function called',(event.target as Element).className);
   }
 }
